fbb5470a0911943e1ff28d58c486ff5fef646bf3
Rob1nFused <robinbos123@gmail.com>
2020-09-22 15:18:36 +0200

    serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
+const moment = require('moment');
 
 
 module.exports.run = async (bot, message, args) => {
 
-    const server = bot.guilds.get('750126432872562788');
-    const serverLeden = server.memberCount;
-    const serverKanalen = '';
-    const serverRollen = '';
+    const serverLeden = message.guild.memberCount;
+    const serverKanalen = message.guild.channels.cache;
+    const serverRollen = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
 
     var serverinfoEmbed = new Discord.MessageEmbed()
 	.setTitle("Server info | NovaHosting")
 	.setColor("RANDOM")
-	.setThumbnail(message.guild.iconURL)
+	.setThumbnail(message.guild.iconURL({ dynamic: true}))
 	.addFields(
         {name: 'Naam', value: 'NovaHosting', inline: true},
         {name: 'ID', value: '750126432872562788', inline: true},
@@ -19,9 +19,12 @@ module.exports.run = async (bot, message, args) => {
     )
     .addFields(
         {name: 'Leden', value: `${serverLeden}`, inline: true},
-        {name: 'Kanalen', value: `${serverKanalen}`, inline: true},
-        {name: 'Rollen', value: `${serverRollen}`, inline: true}
-    );
+        {name: 'Kanalen', value: `${serverKanalen.size}`, inline: true},
+        {name: 'Rollen', value: `${serverRollen.length}`, inline: true}
+    )
+    .addField({name: 'Creatie datum', value: `${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`})
+
+    message.channel.send(serverinfoEmbed);
 	
 
 }
